109fe937b8284363a829090d662e885b81baae15
Shantanu Acharya <thegeek.004@gmail.com>
2020-08-15 20:07:16 +0530

    face alignment deployment

---
+++ 03 - Face Recognition Part 1/deployment/face_alignment/face_alignment.py
@@ -0,0 +1,47 @@
+import dlib
+import cv2
+import numpy as np
+from PIL import Image
+
+import face_blend_common as fbc
+
+
+def load_image(image_bytes):
+    print('Reading image')
+    img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    return img
+
+
+def load_models(predictor_path):
+    print('Initializing the face detector instance')
+    face_detector = dlib.get_frontal_face_detector()
+
+    print('Loading the shape predictor model')
+    landmark_detector = dlib.shape_predictor(predictor_path)
+
+    return face_detector, landmark_detector
+
+
+def normalize_image(img, points):
+    print('Aligning Image')
+    img_norm, points = fbc.normalizeImagesAndLandmarks((600, 600), img, points)
+    img_norm = np.uint8(img_norm * 255)
+    return img_norm
+
+
+def align_image(predictor_path, image_bytes):
+    # Load models and image
+    face_detector, landmark_detector = load_models(predictor_path)
+    img = load_image(image_bytes)
+
+    # Detect landmarks
+    points = np.array(fbc.getLandmarks(face_detector, landmark_detector, img))
+
+    # Convert image to floating point in the range 0 to 1
+    img = np.float32(img) / 255.0
+
+    # Align image
+    output = normalize_image(img, points)
+
+    return Image.fromarray(output)
---
+++ 03 - Face Recognition Part 1/deployment/face_alignment/handler.py
@@ -0,0 +1,68 @@
+try:
+    import unzip_requirements
+except ImportError:
+    pass
+
+import os
+import io
+import json
+import base64
+import boto3
+from requests_toolbelt.multipart import decoder
+
+from face_alignment import align_image
+
+
+# Define environment variables
+MODEL_PATH = 'shape_predictor_5_face_landmarks.dat'
+
+
+def fetch_input_image(event):
+    print('Fetching Content-Type')
+    if 'Content-Type' in event['headers']:
+        content_type_header = event['headers']['Content-Type']
+    else:
+        content_type_header = event['headers']['content-type']
+    print('Loading body...')
+    body = base64.b64decode(event['body'])
+    print('Body loaded')
+
+    # Obtain the final picture that will be used by the model
+    picture = decoder.MultipartDecoder(body, content_type_header).parts[0]
+    print('Picture obtained')
+    
+    return picture.content
+
+
+def align_face(event, context):
+    """Align the Input Image."""
+    try:
+        # Get image from the request
+        picture = fetch_input_image(event)
+
+        # Align image
+        output = align_image(MODEL_PATH, picture)
+        buffer = io.BytesIO()
+        output.save(buffer, format="JPEG")
+        output_bytes = base64.b64encode(buffer.getvalue())
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'image/jpeg',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True
+            },
+            'body': output_bytes
+        }
+    except Exception as e:
+        print(repr(e))
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True
+            },
+            'body': json.dumps({'error': repr(e)})
+        }
